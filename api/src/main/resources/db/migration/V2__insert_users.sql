7d001200f0f515d765fbbdbb8845b46089fc35ab
shrowd <toksycznyburak@gmail.com>
2025-02-11 12:45:21 +0100

    fix: change role value in inserts from Admin to ADMIN

--- api/src/main/resources/db/migration/V2__insert_users.sql
+++ api/src/main/resources/db/migration/V2__insert_users.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (first_name, last_name, email, password, role)
 VALUES
     ('John', 'Doe', 'jd@test.com', '$2a$10$wdianswUbYnEYQdwKIOAgeEsmRLw412vvnnebpdYyCZ3TYJ/H6fMK', 'USER'),
-    ('Alice', 'Smith', 'as@test.com', '$2a$10$wdianswUbYnEYQdwKIOAgeEsmRLw412vvnnebpdYyCZ3TYJ/H6fMK', 'Admin');
+    ('Alice', 'Smith', 'as@test.com', '$2a$10$wdianswUbYnEYQdwKIOAgeEsmRLw412vvnnebpdYyCZ3TYJ/H6fMK', 'ADMIN');
